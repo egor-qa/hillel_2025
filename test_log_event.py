e76a6ca0d33dfe24c501fc137fbf5719755fd859
yehor_qa <84294471+egor-qa@users.noreply.github.com>
2025-07-29 17:12:38 +0300

    Update test_log_event.py

--- test_log_event.py
+++ test_log_event.py
@@ -1,27 +1,42 @@
+import os
 import pytest
 import logging
-from lesson_10.homework10 import (
-    log_event,
-)
+from homework10 import log_event
+
+log_file = "login_system.log"
+
+@pytest.fixture(autouse=True)
+def clear_log_file():
+    """Clear log before test."""
+    if os.path.exists(log_file):
+        os.remove(log_file)
+    # Clear log
+    logger = logging.getLogger("log_event")
+    logger.handlers.clear()
 
 
 @pytest.mark.parametrize(
     "status,expected_level",
     [
-        ("success", logging.INFO),
-        ("expired", logging.WARNING),
-        ("failed", logging.ERROR)
+        ("success", "INFO"),
+        ("expired", "WARNING"),
+        ("failed", "ERROR"),
+        ("unknown", "ERROR"),  # extra case
     ]
 )
-def test_log_event_logging_levels(caplog, status, expected_level):
+def test_log_event_appends_to_log_file(status, expected_level):
     username = "Yehor"
-    expected_msg = f"Login event - Username: {username}, Status: {status}"
+    expected_message = f"Login event - Username: {username}, Status: {status}"
 
-    # caplog - captures logger messages
-    with caplog.at_level(logging.DEBUG):  # covering all levels
-        log_event(username, status)
+    log_event(username, status)
 
-    # Search messages in caplog.records
-    messages = [record for record in caplog.records if expected_msg in record.message]
-    assert len(messages) == 1, f"Expected one log for '{status}'"
-    assert messages[0].levelno == expected_level
+    assert os.path.exists(log_file), "Log file was not created."
+
+    with open(log_file, "r", encoding='utf-8') as file:
+        lines = file.readlines()
+
+    # Останній запис
+    last_entry = lines[-1].strip()
+
+    assert expected_message in last_entry, f"Expected message not found: {last_entry}"
+    assert expected_level in last_entry, f"Expected level {expected_level} not found: {last_entry}"
